61d1dda3eb9f80207bd4f325d96bf2b2d1cd69e1
Małgorzata Kurach <96025745+mkurach@users.noreply.github.com>
2023-07-26 16:50:52 +0200

    calculating teff parameter for stable particles

--- compParticlesMt.c
+++ compParticlesMt.c
@@ -40,6 +40,14 @@ Double_t fitFunctionMtFullY(Double_t *x, Double_t *par) { // 0 - Teff, 1 - norma
         return 0;
 }
 
+Double_t fitFunctionMt(Double_t *x, Double_t *par) {
+    if(x[0] >= 0 && x[0] <= 3)
+        return par[1]*TMath::Exp(-x[0]/par[0]);
+    else    
+        return 0;
+}
+
+
 void createHistograms(int i) {
 
     TH1D* histMt[_N_PARTICLES_][_N_FIGURES_];
@@ -58,7 +66,7 @@ void createHistograms(int i) {
             for(int k= 0; k < _N_FIGURES_; k++) {
             
                 histMt[j][k] = (TH1D*)file->Get(Form("%s%s",histNames[j].Data(),figuresNames[k].Data()));
-                histMt[j][k]->Sumw2();
+                //histMt[j][k]->Sumw2();
                 histMt[j][k] = (TH1D*)histMt[j][k]->Clone(Form("%s%s%s",histNames[j].Data(),casesNames[i].Data(),figuresNames[k].Data()));
                 //can[i][j]->cd();
                 //histMt[i][j]->Draw();
@@ -70,30 +78,50 @@ void createHistograms(int i) {
     //FUNCTIONS
 
     Int_t nparams = 2;
-    TF1* fun[_N_FIGURES_];
-    //Double_t teffParameter[_N_PARTICLES_] = {0.129759,0.101,0.076};
-    //Double_t normParameter[_N_PARTICLES_] = {2.2e6,1e3,1.4e5};
+    //TF1* fun[_N_PARTICLES_][_N_FIGURES_];
+    Double_t teffParameter[_N_PARTICLES_] = {0.129759,0.101,0.076};
+    Double_t normParameter[_N_PARTICLES_] = {2.2e6,1e3,1.4e5};
     //Double_t normParameter[_N_PARTICLES_] = {4.76e10,3.7e6,8e7};
 
-    
-    fun[0] = new TF1("fun0",fitFunctionMtMidY,1,3,nparams);
-    fun[1] = new TF1("fun1",fitFunctionMtFullY,1,3,nparams);
+    /*for(int j = 0; j < _N_PARTICLES_; j ++) {
+        fun[j][0] = new TF1(Form("fun%d0",j),fitFunctionMtMidY,1,3,nparams);
+        fun[j][1] = new TF1(Form("fun%d1",j),fitFunctionMtFullY,1,3,nparams);
+        for (int k = 0; k < _N_FIGURES_; k++) {
+            fun[j][k]->SetParameter(0,teffParameter[j]);
+            fun[j][k]->SetParameter(1,normParameter[j]);
+        }
+    }*/
+
+    /*TF1* fun[_N_PARTICLES_];
+    fun[0] = new TF1("fun0",fitFunctionMtMidY,0,3,nparams);
+    fun[1] = new TF1("fun1",fitFunctionMtFullY,0,3,nparams);
 
     for(int j = 0; j < _N_FIGURES_; j++) {
-        fun[j]->SetParameter(0,0.1);
-        fun[j]->SetParameter(1,5e7);
+        fun[j]->SetParameter(0,0.00001);
+        fun[j]->SetParameter(1,2e3);
+        fun[j]->SetParLimits(0,0.00001,0.001);
+        fun[j]->SetParLimits(1,100,10000);
+    }*/
+
+    TF1* fun[_N_PARTICLES_];
+    for(int j = 0; j < _N_PARTICLES_; j++) {
+        fun[j] = new TF1(Form("fun%d",j),fitFunctionMt,0,3,nparams);
+        fun[j]->SetParameter(0,teffParameter[j]);
+        fun[j]->SetParameter(1,normParameter[j]);
     }
+    
+
    
 
     //FIITING
 
-    Double_t fitMin[_N_CASES_][_N_PARTICLES_] = {{0.25,0.3,0.15},
-                                                {0.4,0.3,0.15},
-                                                {0.4,0.3,0.15}};
+    Double_t fitMin[_N_CASES_][_N_PARTICLES_] = {{1.15,0.6,0.2},
+                                                {1.15,0.6,0.2},
+                                                {1.15,0.6,0.2}};
 
-    Double_t fitMax[_N_CASES_][_N_PARTICLES_] = {{1.8,1,0.7},
-                                                {1.8,1,0.7},
-                                                {1.8,1,0.7}};
+    Double_t fitMax[_N_CASES_][_N_PARTICLES_] = {{1.7,0.9,0.55},
+                                                {1.7,0.9,0.55},
+                                                {1.7,0.9,0.55}};
 
     TF1* funCop;
     Double_t minRange[_N_PARTICLES_] ={0,0,0};
@@ -109,16 +137,16 @@ void createHistograms(int i) {
     TFile* fileOut = new TFile(Form("outputPart/outFitMtPart%s.root",casesNames[i].Data()),"RECREATE");
 	fileOut->cd();
 
-    TCanvas* can[_N_PARTICLES_][_N_FIGURES_];
+    //TCanvas* can[_N_PARTICLES_][_N_FIGURES_];
     //for(int i = 0; i < _N_CASES_; i++) {
     //for(int i = 0; i < 1; i++) {
         for(int j = 0; j < _N_PARTICLES_; j++) { 
             for(int k = 0; k < _N_FIGURES_; k++) {
-                can[j][k] = new TCanvas(Form("%s%s%s",casesNames[i].Data(),particlesTitles[j].Data(),figuresNames[k].Data()),Form("%s%s%s",casesNames[i].Data(),particlesTitles[j].Data(),figuresNames[k].Data()),1000,1000);
-                histMt[j][k]->Fit(fun[k],"Q","Q",fitMin[i][j],fitMax[i][j]);
-
-                funCop = (TF1*)fun[k]->Clone("funCop");
-                funCop->SetRange(minRange[j],maxRange[j]);
+                //can[j][k] = new TCanvas(Form("%s%s%s",casesNames[i].Data(),particlesTitles[j].Data(),figuresNames[k].Data()),Form("%s%s%s",casesNames[i].Data(),particlesTitles[j].Data(),figuresNames[k].Data()),1000,1000);
+                histMt[j][k]->Fit(fun[j],"","",fitMin[i][j],fitMax[i][j]);
+//Form("fun%i%i",j,k)
+                funCop = (TF1*)fun[j]->Clone("funCop");
+                funCop->SetRange(mXValues[j],maxRange[j]);
                 funCop->SetLineStyle(2);
                 histMt[j][k]->GetListOfFunctions()->Add(funCop);
 
@@ -128,8 +156,8 @@ void createHistograms(int i) {
                 //funCop[j]->Draw("same");
                 histMt[j][k]->Write();
 
-                tEff[j][k] = fun[k]->GetParameter(0);
-                tEffEr[j][k] = fun[k]->GetParError(0);
+                tEff[j][k] = fun[j]->GetParameter(0);
+                tEffEr[j][k] = fun[j]->GetParError(0);
             }
         }
     //}
@@ -163,9 +191,9 @@ void createHistograms(int i) {
 
 void compParticlesMt() {
 
-    createHistograms(0);
+    //createHistograms(0);
     //createHistograms(1);
-    //createHistograms(2);
+    createHistograms(2);
 
 }
 
